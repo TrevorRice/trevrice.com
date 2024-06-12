0a46ac17eb744504d67097f4689e170d77a4e7e5
Trevor Rice <trevor.rice@hey.com>
2024-06-11 21:16:19 -0500

    Update 2024

--- .eleventy.js
+++ .eleventy.js
@@ -1,11 +1,9 @@
 module.exports = (eleventyConfig) => {
-  eleventyConfig.setTemplateFormats([
-    'html',
-    'css',
-    'jpg',
-    'png',
-    'svg',
-    'mp4',
-    'ttf'
-  ])
+  eleventyConfig.addPassthroughCopy("src/**/*")
+
+  return {
+    dir: {
+      input: "src"
+    }
+  }
 }
